52936589b11c52b5cf9ad985d613b7eee9b66fcf
JY <77328954+JYBloger@users.noreply.github.com>
2023-02-19 15:01:14 +0800

    Delete src directory

--- src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import { routes } from "./route";
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
-
-export default router;
